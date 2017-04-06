5bc573336af5b7e64ab25b9eccdbbb9cfcac89a4
srimuthurajesh <srimuthurajesh@gmail.com>
2017-04-07 00:38:16 +0530

    Tkinter gui

---
+++ MultipleInheritance.py
@@ -0,0 +1,10 @@
+Class BaseClass_A(object):
+    def hello(self)
+        print("vanakam")
+Class BaseClass_B(object):
+    def hello(self)
+        print("vanakam")
+Class ChildClass(BaseClass_A,BaseClass_B):
+
+obj=ChildClass()
+print(obj.hello())
---
+++ MyFirstGui.py
@@ -0,0 +1,5 @@
+from tkinter import*
+root=Tk()
+label=Label(root,text="Hello Gui, iam here to rock it")
+label.pack()
+root.mainloop()
