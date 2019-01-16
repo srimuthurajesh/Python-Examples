6c211d3fd797973ab6c8a284b09cdd36c99356fe
srimuthurajesh <srimuthurajesh@gmail.com>
2019-01-16 18:56:24 +0530

    using iris dataset implementing classification algo

---
+++ iris_dataset_classification.py
@@ -0,0 +1,32 @@
+from sklearn.datasets import load_iris
+
+iris = load_iris()
+#print(iris.target)
+#print(iris.data)
+X=iris.data
+y=iris.target
+#print(X.shape)
+#print(y.shape)
+
+#importing KNN library
+from sklearn.neighbors import KNeighborsClassifier
+
+knn = KNeighborsClassifier(n_neighbors=1)
+#print(knn)
+knn.fit(X, y)
+print(knn.predict([[3,5,4,2]]))
+print(knn.predict([[3,5,4,2],[5,4,3,2]]))
+
+#use different neightbor value
+knn = KNeighborsClassifier(n_neighbors=2)
+#print(knn)
+knn.fit(X, y)
+print(knn.predict([[3,5,4,2]]))
+print(knn.predict([[3,5,4,2],[5,4,3,2]]))
+
+#import clssification model
+from sklearn.linear_model import LogisticRegression
+logreg = LogisticRegression()
+logreg.fit(X,y)
+print(logreg.predict([[3,5,4,2]]))
+print(logreg.predict([[3,5,4,2],[5,4,3,2]]))
